903a41b2161d6dc86379e81bc226b7664af8bc24
Robert Obloj <robloj@gmail.com>
2018-08-08 18:24:39 +0200

    images for ubuntu

---
+++ ubuntu/run.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set build=%1
+SET build_dir=c:/tmp/output/ubuntu
+
+if "%~1"=="build" (
+    docker-compose up --build -d tensorflow-builder
+    echo "Builder up with build option"
+) else (
+    docker-compose up -d tensorflow-builder
+    echo "Builder up"
+)
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker-compose ps -q tensorflow-builder`) DO (
+    SET container_id=%%F
+)
+
+if not exist %build_dir% mkdir %build_dir%
+docker cp %container_id%:/tmp/tensorflow_pkg %build_dir%
+docker-compose down
+
+if exist %build_dir%/tensorflow_pkg (
+    echo "Tensorflow compiled and saved in dir: %build_dir%"
+)
